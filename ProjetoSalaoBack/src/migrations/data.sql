1ec79a051df9cc1afec2feff803aed74be5569ea
Lupalufa <lupalufa431@gmail.com>
2025-04-02 17:19:30 -0300

    feat: adição da dependência axios e criação das tabelas e rotas para gerenciamento de agendamentos, clientes e endereços

---
+++ ProjetoSalaoBack/src/migrations/data.sql
@@ -0,0 +1,37 @@
+create table if not exists salao_agendamento(
+	id serial primary key,
+	id_cliente int not null,
+	id_endereco int not null,
+	servico varchar(50) not null,
+	forma_pagamento varchar(30) not null,
+	observacoes text,
+	confirmacao varchar(20),
+	constraint fk_cliente_agendamento_id foreign key (id_cliente) references salao_cliente(id),
+	constraint fk_endereco_agendamento_id foreign key (id_endereco) references salao_enderecos(id)
+);
+
+
+
+create table if not exists salao_cliente (
+	id serial primary key,
+	nome varchar(40) not null,
+	idade int not null,
+	contato char(15),
+	data_hora timestamp
+);
+
+
+
+create table if not exists salao_enderecos (
+	id serial primary key,
+	id_cliente int not null,
+	cep char(8) not null,
+	logradouro varchar(60),
+	numero varchar(10) not null,
+	complemento varchar(60),
+	bairro varchar(30) not null,
+	localidade varchar(50) not null,
+	uf varchar(2) not null,
+	ponto_referencia text,
+	constraint fk_id_cliente foreign key (id_cliente) references salao_cliente(id)
+);
---
+++ ProjetoSalaoBack/src/modules/Endereco/models/index.js
@@ -0,0 +1,41 @@
+const { pool } = require("../../../config/database")
+const axios = require("axios")
+
+class EnderecoModel {
+    static async criarEndereco(id, cep, numero, ponto_referencia){
+        const resposta = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+        const {logradouro, complemento, bairro, localidade, uf} = resposta.data
+        const dados = [id, cep, numero, ponto_referencia, logradouro, complemento, bairro, localidade, uf]
+        const consulta = `insert into`
+    }
+
+    static async listarEndereco(){
+        const consulta = `select * from salao_enderecos`
+        const resultado = pool.query(consulta)
+        return resultado.rows
+    }
+
+    static async listarEnderecoCliente(cliente){
+        const dados = [cliente]
+        const consulta = `
+        select cliente.*, endereco.* 
+        from salao_enderecos join salao_cliente 
+        on salao_cliente.id = salao_enderecos.id_cliente`
+        const resultado = pool.query(consulta, dados)
+        return resultado.rows
+    }
+
+    static async listarEnderecoCidade(cidade){
+        const dados = [`%${cidade}%`]
+        const consulta = `select * from salao_enderecos where lower(logradouro) like lower($1)`
+        const resultado =  pool.query(consulta, dados)
+        return resultado.rows
+    }
+
+    static async editarEndereco(id, cep, numero, ponto_referencia){
+        const resposta = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+        const {logradouro, complemento, bairro, localidade, uf} = resposta.data
+        const dados = [id, cep, numero, ponto_referencia, logradouro, complemento, bairro, localidade, uf]
+        const consulta = `update salao_endereco set cep = $ , numero = $ , ponto_referencia = $ , logradouro = $ , complemento = $ , bairro = $ , localidade = $ , uf = $`
+    }
+}
